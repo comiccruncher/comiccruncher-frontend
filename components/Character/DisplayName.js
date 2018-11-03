c9d230575e386cdb86f94b45a07d452069773bf6
Aimee LaPlant <aimee@aimeelaplant.com>
2018-11-03 03:10:25 -0400

    Check the issue count exists first

--- components/Character/DisplayName.js
+++ components/Character/DisplayName.js
@@ -6,6 +6,11 @@ export const DisplayName = (props) => (
   <div className="DisplayName">
     <Text.Default bold>{props.name}</Text.Default>
     <Text.Default>{props.other_name}</Text.Default>
-    <Text.Default>#{props.issue_count_rank}, {props.issue_count} issues</Text.Default>
+    {props.issue_count &&
+      props.issue_count_rank && (
+        <Text.Default>
+          #{props.issue_count_rank}, {props.issue_count} issues
+        </Text.Default>
+      )}
   </div>
 );
