5f2b36d5deecadfa0480a9a411700337689f6ab6
Aimee LaPlant <aimee@aimeelaplant.com>
2018-11-13 23:09:08 -0500

    Updates to header and character page

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -3,30 +3,41 @@ import styled from 'react-emotion';
 import { UI } from '../shared/styles/colors';
 import Responsive from '../shared/styles/responsive';
 import Spacing from '../shared/styles/spacing';
+import Lines from '../shared/components/Lines';
 
-const HeaderSection = styled('div')({
-  background: UI.Background.RedGradient,
+const HeaderSection = styled('div')((props) => ({
+  overflow: 'hidden',
+  position: 'relative',
+  background: props.background || UI.Background.RedGradient,
   minHeight: '420px',
-  textAlign: 'center',
-  padding: Spacing.xxLarge,
+  textAlign: props.textAlign || 'center',
+  padding: Spacing.xxxLarge,
   [Responsive.Mobile]: {
     padding: Spacing.Large,
   },
-});
+}));
+
+const GenericHeader = styled('div')((props) => ({
+  background: props.background,
+  position: 'relative',
+  overflow: 'hidden',
+  padding: props.padding || 0,
+  textAlign: props.textAlign || 'center',
+}));
 
 // A generic fluid header with options to customize the CSS.
 export const Header = (props) => (
   <React.Fragment>
-    <div style={{ background: `${props.bg}`, position: 'relative', overflow: 'hidden', padding: props.padding || 0 }}>
-      <header style={{ padding: '0' }}>{props.children}</header>
-    </div>
+    <GenericHeader {...props}>
+      <header>{props.children}</header>
+    </GenericHeader>
   </React.Fragment>
 );
 
 // The main header for the frontpage.
 export const MainHeader = (props) => (
   <React.Fragment>
-    <HeaderSection>
+    <HeaderSection background={props.background} textAlign={props.textAlign}>
       <header>{props.children}</header>
     </HeaderSection>
   </React.Fragment>
