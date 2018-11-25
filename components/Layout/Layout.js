35c6ab0d9f8a9679a2ad3f4d5f551e7222948131
Aimee LaPlant <aimee@aimeelaplant.com>
2018-11-25 14:35:09 -0500

    Added cool loading bar

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
 import { injectGlobal } from 'emotion';
-import { UI } from '../shared/styles/colors';
+import { UI, Palette } from '../shared/styles/colors';
 import Spacing from '../shared/styles/spacing';
-import PropTypes from 'prop-types';
 import Navigation from './Navigation';
 import { withCache } from '../emotion/cache';
 import Responsive from '../shared/styles/responsive';
@@ -197,6 +196,78 @@ injectGlobal`
     opacity: 0;
     animation: blowUpModalTwo 0.5s cubic-bezier(0.165, 0.84, 0.44, 1) forwards;
   }
+
+#nprogress {
+  pointer-events: none;
+}
+
+#nprogress .bar {
+  background: ${Palette.Yellow.Default};
+  position: fixed;
+  z-index: 1031;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 5px;
+}
+
+/* Fancy blur effect */
+#nprogress .peg {
+  display: block;
+  position: absolute;
+  right: 0px;
+  width: 100px;
+  height: 100%;
+  box-shadow: 0 0 10px ${Palette.Yellow.Default}, 0 0 5px ${Palette.Yellow.Default};
+  opacity: 1.0;
+
+  -webkit-transform: rotate(3deg) translate(0px, -4px);
+      -ms-transform: rotate(3deg) translate(0px, -4px);
+          transform: rotate(3deg) translate(0px, -4px);
+}
+
+/* Remove these to get rid of the spinner */
+#nprogress .spinner {
+  display: block;
+  position: fixed;
+  z-index: 1031;
+  top: 15px;
+  right: 15px;
+}
+
+#nprogress .spinner-icon {
+  width: 18px;
+  height: 18px;
+  box-sizing: border-box;
+
+  border: solid 2px transparent;
+  border-top-color: ${Palette.Yellow.Default};
+  border-left-color: ${Palette.Yellow.Default};
+  border-radius: 50%;
+
+  -webkit-animation: nprogress-spinner 400ms linear infinite;
+          animation: nprogress-spinner 400ms linear infinite;
+}
+
+.nprogress-custom-parent {
+  overflow: hidden;
+  position: relative;
+}
+
+.nprogress-custom-parent #nprogress .spinner,
+.nprogress-custom-parent #nprogress .bar {
+  position: absolute;
+}
+
+@-webkit-keyframes nprogress-spinner {
+  0%   { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+@keyframes nprogress-spinner {
+  0%   { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
 `;
 
 const Layout = (props) => (
