3efee609b1617ad8ee7729eeff9ce5d25cccd713
Aimee LaPlant <aimee@aimeelaplant.com>
2018-12-27 12:47:04 -0500

    Remove extra {

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -287,7 +287,7 @@ const Layout = ({ canonical, children, title, navBackground }) => (
       <link rel="apple-touch-icon-precomposed" sizes="57x57" href={`${cdnURL}/apple-touch-icon-57x57.png`} />
       <link rel="apple-touch-icon-precomposed" sizes="114x114" href={`${cdnURL}/apple-touch-icon-114x114.png`} />
       <link rel="apple-touch-icon-precomposed" sizes="72x72" href={`${cdnURL}/apple-touch-icon-72x72.png`} />
-      <link rel="apple-touch-icon-precomposed" sizes="144x144" href={`{${cdnURL}/apple-touch-icon-144x144.png`} />
+      <link rel="apple-touch-icon-precomposed" sizes="144x144" href={`${cdnURL}/apple-touch-icon-144x144.png`} />
       <link rel="apple-touch-icon-precomposed" sizes="60x60" href={`${cdnURL}/apple-touch-icon-60x60.png`} />
       <link rel="apple-touch-icon-precomposed" sizes="120x120" href={`${cdnURL}/apple-touch-icon-120x120.png`} />
       <link rel="apple-touch-icon-precomposed" sizes="76x76" href={`${cdnURL}/apple-touch-icon-76x76.png`} />
