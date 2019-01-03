094b46ae29a82c0adcb5631a1e6727d269587def
Aimee LaPlant <aimee@aimeelaplant.com>
2019-01-02 21:59:45 -0500

    WHY

--- server.js
+++ server.js
@@ -1,22 +1,51 @@
 const express = require('express');
 const next = require('next');
 const dev = process.env.NODE_ENV !== 'production';
+const authSecret = process.env.CC_JWT_AUTH_SECRET;
 const app = next({ dev });
 const handle = app.getRequestHandler();
+const cookieParser = require('cookie-parser');
+const axios = require('axios');
+
+const getToken = () => {
+  try {
+    return axios({
+      method: 'get',
+      url: 'http://localhost:8001/authenticate',
+      headers: {
+        Authorization: `Bearer ${authSecret}`,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const jwtMiddleware = async (req, res, next) => {
+  if (!req.cookies || !req.cookies.cc_session_id) {
+    const data = await getToken();
+    // UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). (rejection id: 1)
+    //  WHY? I CAUGHT THE ERROR!!!!!!!!
+    if (data) {
+      const t = data.data.token;
+      res.cookie('cc_session_id', t, { httpOnly: !dev, secure: !dev });
+    }
+  }
+};
 
 app
   .prepare()
   .then(() => {
     const server = express();
-
     server.disable('x-powered-by');
-
+    server.use(cookieParser());
     server.use((req, res, next) => {
       res.setHeader('X-Content-Type-Options', 'nosniff');
       res.setHeader('X-Frame-Options', 'sameorigin');
       res.setHeader('Referrer-Policy', 'origin');
       res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
       res.setHeader('x-xss-protection', '1; mode=block');
+      jwtMiddleware(req, res, next);
       next();
     });
 
@@ -24,34 +53,34 @@ app
       // `/marvel` is the filename of `/pages/marvel.js
       // * must pass in req.params for back button to work:
       // https://github.com/zeit/next.js/issues/3065#issuecomment-423035872
-      app.render(req, res, '/marvel', req.params);
+      app.render(req, res, '/marvel', req);
     });
 
     server.get('/dc', (req, res) => {
-      app.render(req, res, '/dc', req.params);
+      app.render(req, res, '/dc', req);
     });
 
     server.get('/trending', (req, res) => {
-      app.render(req, res, '/trending', req.params);
+      app.render(req, res, '/trending', req);
     });
 
     server.get('/characters/:slug', (req, res) => {
-      app.render(req, res, '/character', req.params);
+      app.render(req, res, '/character', req);
     });
 
     server.get('/faq', (req, res) => {
-      app.render(req, res, '/faq', req.params);
+      app.render(req, res, '/faq', req);
     });
 
     server.get('/privacy', (req, res) => {
-      app.render(req, res, '/privacy', req.params);
+      app.render(req, res, '/privacy', req);
     });
 
     server.get('/', (req, res) => {
       // `/index` is the filename of `/pages/index.js
       // * must pass in req.params for back button to work:
       // https://github.com/zeit/next.js/issues/3065#issuecomment-423035872
-      app.render(req, res, '/index', req.params);
+      app.render(req, res, '/index', req);
     });
 
     server.get('*', (req, res) => {
